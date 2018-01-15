fd343f9de1de8df4fc36257b8b661fb38b5546ab
umetsu <prunusmume0727@gmail.com>
2018-01-15 16:11:46 +0900

    Initial commit

---
+++ src/container/App.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Hello from '../component/Hello';
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Hello/>
+        );
+    }
+}
+
+export default App;
